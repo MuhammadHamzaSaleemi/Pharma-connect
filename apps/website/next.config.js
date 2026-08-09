623fea219696523569b700d0f563ac9bb4d8db60
Muhammad Hamza Saleemi <mrmuhammadhamzasaleemi@gmail.com>
2026-08-10 00:30:39 +0500

    update config

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
 const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
   reactStrictMode: false,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
